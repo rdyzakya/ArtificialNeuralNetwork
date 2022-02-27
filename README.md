c62cbf9a7982bd22fdfcc62f775c66bccd5499cc
widyaput <wnugrah.putra@gmail.com>
2022-02-27 21:42:26 +0700

    wiwid mau ngerjain pbd dulu dada guys

--- README.md
+++ README.md
@@ -1,38 +1,6 @@
 # ArtificialNeuralNetwork
 Tugas Besar Machine Learning
 
-Contoh memakai kodenya:
+Install [poetry](https://python-poetry.org/docs/master/#installing-with-the-official-installer) biar ganteng
 
-```python
-from src.ANN import *
-import numpy as np
-
-
-model = Sequential()
-model.add(Dense(units=2, input_dim=2, activation="relu"))
-model.add(Dense(units=1, activation="sigmoid"))
-
-# only one data point / 1 instance
-input_matrix_1 = np.array([[0,1]])
-
-# five data points / 5 instances
-input_matrix_2 = np.array([[0,1],[1,0],[1,1],[0,0],[1,1]])
-
-res_1 = model.forward_feed(input_matrix_1)
-
-res_2 = model.forward_feed(input_matrix_2)
-
-print("Result with one data point:")
-print(res_1)
-print("Result with five data points:")
-print(res_2)
-
-# modify weights and biases
-weights_1 = np.array([[1,1],[1,1]])
-biases_1 = np.array([1,1])
-model.layers[0].compile_weight_and_bias(weights=weights_1,biases=biases_1)
-
-weights_2 = np.array([[1],[1]])
-biases_2 = np.array([1])
-model.layers[1].compile_weight_and_bias(weights=weights_2,biases=biases_2)
-```
+Terus `poetry install` deh
--- main.py
+++ main.py
@@ -2,9 +2,48 @@ from src.ANN import *
 import numpy as np
 
 if __name__ == '__main__':
+    input_single = np.array([[0, 1]])
+    input_batch = np.array([[0, 1], [1, 0], [0, 0], [1, 1]])
+    
     # Model Sigmoid
     model = Sequential()
     model.add(Dense(units=2, input_dim=2, activation="sigmoid"))
     model.add(Dense(units=1, activation="sigmoid"))
 
+    weight_hidden_layer = np.array([[20, -20], [20, -20]])
+    bias_hidden_layer = np.array([-10, 30])
+    model.layers[0].compile_weight_and_bias(weights=weight_hidden_layer, biases=bias_hidden_layer)
+
+    weight_output_layer = np.array([[20], [20]])
+    bias_output_layer = np.array([-30])
+    model.layers[1].compile_weight_and_bias(weights=weight_output_layer, biases=bias_output_layer)
+
+    res_single = model.predict(input_single)
+
+    print(res_single)
+
+    res_batch = model.predict(input_batch)
     
+    print(res_batch)
+
+    # Model RelU + Linear
+    model = Sequential()
+    model.add(Dense(units=2, input_dim=2, activation="relu"))
+    model.add(Dense(units=1, activation="linear"))
+
+    weight_hidden_layer = np.array([[1, 1], [1, 1]])
+    bias_hidden_layer = np.array([0, -1])
+    model.layers[0].compile_weight_and_bias(weights=weight_hidden_layer, biases=bias_hidden_layer)
+
+    weight_output_layer = np.array([[1], [-2]])
+    bias_output_layer = np.array([0])
+    model.layers[1].compile_weight_and_bias(weights=weight_output_layer, biases=bias_output_layer)
+
+    res_single = model.predict(input_single)
+
+    print(res_single)
+
+    res_batch = model.predict(input_batch)
+    
+    print(res_batch)
+
