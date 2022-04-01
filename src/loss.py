ae44805664cb3569cb40aca54ffda37c38662514
Randy <randomzakya@gmail.com>
2022-04-01 07:54:25 +0700

    delete /N in cross entropy

--- src/loss.py
+++ src/loss.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+epsilon = 1e-12
 
 def sum_squared_error(
     y_true: np.ndarray, y_pred: np.ndarray, derivative: bool = False
@@ -16,41 +17,12 @@ def sum_squared_error(
     """
     if derivative:
         return np.sum(y_true - y_pred,axis=0)
-    return np.sum(np.square(y_true - y_pred)) / 2
-
-def entropy(y_true:float, y_pred:float,derivative=False):
-    """
-    [DESC]
-        Function to calculate entropy
-    [PARAMS]
-        y_true : float
-        y_pred : float
-        derivative : bool
-    [RETURN]
-        float
-    """
-    if not derivative:
-        if y_true == 1:
-            if y_pred == 0:
-                return -1
-            return -np.log(y_pred)
-        else:
-            if y_pred == 1:
-                return -1
-            return -np.log(1-y_pred)
-    if y_true == 1:
-        if y_pred == 0:
-            y_pred += 0.001
-        return -1/y_pred
-    else:
-        if y_pred == 1:
-            y_pred -= 0.001
-        return 1/(1-y_pred)
+    return np.sum(np.square(y_true - y_pred)) / 2 # not axis 0 because this is used only for model.errors
         
 
 
 def cross_entropy_error(
-    y_true: np.ndarray, y_pred: np.ndarray, derivative: bool = False, epsilon:float=1e-12) -> float:
+    y_true: np.ndarray, y_pred: np.ndarray, derivative: bool = False) -> float:
     """
     [DESC]
         Function to calculate cross entropy error
@@ -63,10 +35,7 @@ def cross_entropy_error(
         float
     """
     y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-    # vect = np.vectorize(lambda true, pred, derivative: entropy(true, pred, derivative))
     if derivative:
-        # return np.sum(vect(y_true, y_pred, derivative), axis=0)
         return np.sum(-y_true/y_pred + (1-y_true)/(1-y_pred),axis=0)
-    N = y_pred.shape[0]
-    ce = -np.sum(y_true*np.log(y_pred+1e-9))/N
+    ce = np.sum(-y_true*np.log(y_pred) - (1-y_true)*np.log(1-y_pred)) # not axis 0 because this is used only for model.errors
     return ce
